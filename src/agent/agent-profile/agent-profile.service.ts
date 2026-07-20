02d6f53c28e84773884de90f0b710df74d1d316f
= <kaydeymyrtle@gmail.com>
2026-07-20 20:32:18 +0100

    Finally3

--- src/agent/agent-profile/agent-profile.service.ts
+++ src/agent/agent-profile/agent-profile.service.ts
@@ -148,7 +148,7 @@ export class AgentProfileService {
 	private setTokenCookie(res: Response, token: string) {
 		res.cookie('jwt', token, {
 			httpOnly: true,
-			secure: process.env.ENV_MODE === 'PROD',
+			secure: true,
 			sameSite: 'none',
 			maxAge: 7 * 24 * 60 * 60 * 1000,
 			path: '/',
