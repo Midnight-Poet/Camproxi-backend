a54e729063f4d87f06c1b420cbceaef721a52ba3
= <kaydeymyrtle@gmail.com>
2026-07-27 16:38:18 +0100

    Stage 2 testing: error handling4

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import cookieParser from 'cookie-parser';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   console.log(process.env.AGENT_URL)
